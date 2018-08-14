893e50bdc711712088f7ef3e36a08a5c78d6fc6b
Hingyi  Khong and Krystal Foster <krystal@>
2018-08-14 16:20:59 -0700

    use for loop and branching or statements to extract vowels and split and join

--- js/scripts.js
+++ js/scripts.js
@@ -5,24 +5,30 @@ $(document).ready(function(event) {
 //   $(".yourSentence").text(userInput);
 //   $(".yourSentence").show(userInput);
 //
-  var vowels = ['a', 'e', 'i', 'o', 'u'];
+  // var vowels = ['a', 'e', 'i', 'o', 'u'];
 
   var userInput = $("input#enterInput").val();
-  var noVowelsArr = userInput.split(vowels[i]);
-  var noVowelStr = noVowelsArr.join("-");
+  // var noVowelsArr = userInput.split("i");
+  // var noVowelStr = noVowelsArr.join("-");
 
-  for (var i = 0; i < vowels.length; i++) {
-
-
+  // for (var i = 0; i < vowels.length; i++) {
 
 
+var userArr = userInput.split("");
 
+for (var i = 0; i <= userArr.length; i ++) {
+  if (userArr[i] === "a" || userArr[i] === "e" || userArr[i] === "i" || userArr[i] === "o" || userArr[i] === "u") {
+    userArr.splice(i, 1, "-");
+  }
 }
-
-
-
-
+  var noVowelStr = userArr.join("");
   $(".yourSentence").text(noVowelStr);
+});
+
+
+
+
+  // $(".yourSentence").text(noVowelStr);
 //
 //   var vowelled = $("Your i sentence here!");
 //   var devolvingArr = vowelled.split("i");
@@ -48,5 +54,5 @@ $(document).ready(function(event) {
   // var sentenceString = ["Believe you can and you're halfway there."]
 
   // var extractedVowels = sentenceString.map(function(vowel) {//   return vowel
-  });
-});
+  // });
+ });
